56bc8f2eb42ccf73a8551d9618c0ae277ae011b8
Mihai Dinca <dincamihai@gmail.com>
2016-01-08 02:39:24 +0200

    Add threshold config param

--- salt/roots/_beacons/cpu.py
+++ salt/roots/_beacons/cpu.py
@@ -27,23 +27,39 @@ def __virtual__():
 
 
 def validate(config):
-    '''
-    Validate the beacon configuration
-    '''
+    """Validate the beacon configuration."""
+    if not isinstance(config, dict):
+        log.info('Configuration for cpu beacon must be a dictionary.')
+        return False
+    else:
+        threshold = config.get('threshold', None)
+        if threshold is not None:
+            try:
+                config['threshold'] = float(threshold)
+            except ValueError:
+                log.info('threshold must be a numeric value.')
+                return False
     return True
 
 
 def beacon(config):
-    '''
-    Watch the configured files
+    """ Report CPU% usage.
 
     Example Config
 
     .. code-block:: yaml
 
         beacons:
-          test: {}
-    '''
-    ret = [{"cpu%": psutil.cpu_percent()}]
-    # Return event data
+          cpu: {
+            threshold: 80
+          }
+    """
+    ret = []
+    threshold = config.get('threshold', None)
+    data = {"cpu%": psutil.cpu_percent()}
+    if threshold is None:
+        ret.append(data)
+    else:
+        if data["cpu%"] >= threshold:
+            ret.append(data)
     return ret
